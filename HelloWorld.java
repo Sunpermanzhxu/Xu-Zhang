8e040518cacafded94bd32298681f8a14f2b76d0
Xu Zhang <69454371+Sunpermanzhxu@users.noreply.github.com>
2021-04-22 10:16:27 +1000

    push test 01
    not an official work

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+   public static void main(String[] args) {
+       // Prints "Hello, World" to the terminal window.
+       System.out.println("Hello World!");
+       System.out.println("This is COSC2803 - Programming Studio 1");
+   }
+
+   // change test 01
+
+}
